dfae25e3b8b1f3b1839015c3cd6545b0fcb28044
hippo <hippo.finance.com@gmail.com>
2020-10-19 23:58:22 +0900

    no message

---
+++ contracts/Burn.sol
@@ -0,0 +1,36 @@
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./Executor.sol";
+import "./FundWallet.sol";
+
+contract Burn is VoteExecutor {
+
+    address public aHIPPO;
+    address public dHIPPO;
+    address public fundAddr;
+
+    constructor(address _aHIPPO, address _dHIPPO, address _fundAddr) public {
+        aHIPPO = _aHIPPO;
+        dHIPPO = _dHIPPO;
+    }
+
+    function setAHippo(address addr_) public onlyOwner {
+        aHIPPO = addr_;
+    }
+
+    function setDHippo(address addr_) public onlyOwner {
+        dHIPPO = addr_;
+    }
+
+    function executeVote() public {
+        FundWallet fundContract = FundWallet(fundAddr);
+        ERC20 ahpo = ERC20(aHIPPO);
+        ERC20 dhpo = ERC20(dHIPPO);
+        uint256 aHippoBalance = ahpo.balanceOf(address(this));
+        fundContract.fund(aHIPPO, address(0), aHippoBalance);
+        uint256 dHippoBalance = dhpo.balanceOf(address(this));
+        fundContract.fund(dHIPPO, address(0), dHippoBalance);
+    }
+
+}
---
+++ contracts/Executor.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract VoteExecutor is Ownable {
+
+    constructor() public {
+
+    }
+
+    function executeVote() public {
+
+    }
+
+}
